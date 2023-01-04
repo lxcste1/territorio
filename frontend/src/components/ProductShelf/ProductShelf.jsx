74e8af1142f8c964cc6da1a9408e3bbd91743550
tellol <lucas@tobs.com.ar>
2023-01-04 20:33:59 -0300

    Pruebas de categorias

--- frontend/src/components/ProductShelf/ProductShelf.jsx
+++ frontend/src/components/ProductShelf/ProductShelf.jsx
@@ -4,18 +4,14 @@ import styles from '../../styles/ProductShelf.module.css'
 
 export default function ProductShelf({ type }) {
 
-    console.log(type)
-
     const {data, loading, error} = useFetch(`api/products?populate=*&[filters][type][$eq]=${type}`);
-
-    console.log(data)
 
     return (
         <>  
             <h2>{type}</h2>
             <div className={styles.shelf}>
             {
-            loading ? "cargando productos" : data?.map((product)=> {
+            error ? "Algo salió mal :(" : loading ? "Cargando productos" : data?.map((product)=> {
                 return (
                 <>
                     <ProductCard attributes={product}></ProductCard>
--- frontend/src/pages/servicios/index.jsx
+++ frontend/src/pages/servicios/index.jsx
@@ -2,26 +2,35 @@ import { Container } from "react-bootstrap";
 import styles from '../../styles/Servicios.module.css';
 import ProductShelf from "../../components/ProductShelf/ProductShelf";
 import useFetch from '../../hooks/useFetch';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Servicios () {
 
   const {data} = useFetch("api/categories/")
 
-  const [type, setType] = useState([]);
+  const pepe = data;
+
+  const [info, setInfo] = useState(pepe)
+
+  useEffect(() => {
+    console.log("se dispara el efecto")
+
+    console.log(info)
+  }, [])
 
   return (
   <div>
     <Container>
       <h1 className={styles.ServicesTitle}>Cuidado & belleza</h1>
-      {/* {
+      <h2>¿Qué servicio querés ver?</h2>
+      {
         data?.map((attributes)=> {
-          console.log(attributes.attributes.name);
-          setType(attributes.attributes.name);
+          const typeFilter = attributes.attributes.name;
+          const typeId = attributes.id;
+
+          return (<ProductShelf type={typeFilter} key={typeId}></ProductShelf>)
         })
-      } */}
-    <ProductShelf type="Manicuría"></ProductShelf>
-    <ProductShelf type="Masajes"></ProductShelf>
+      }
   </Container>
   </div>
   );
